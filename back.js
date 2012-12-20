e0e9303881688aaaa6283f9716540eeb97860484
amit <amit@amit-laptop.(none)>
2012-12-19 19:11:23 -0500

    added loop instead of 10 statements :|

--- back.js
+++ back.js
@@ -1,25 +1,13 @@
-var x = $("a:contains('Sign Out')").attr('href');
-if (! x)
-var x = $("a:contains('Sign out')").attr('href');
-if (! x)
-var x = $("a:contains('sign out')").attr('href');
-if (! x)
-var x = $("a:contains('signout')").attr('href');
-if (! x)
-var x = $("a:contains('Signout')").attr('href');
-if (! x)
-var x = $("a:contains('logout')").attr('href');
-if (! x)
-var x = $("a:contains('log out')").attr('href');
-if (! x)
-var x = $("a:contains('Logout')").attr('href');
-if (! x)
-var x = $("a:contains('Log Out')").attr('href');
-if (! x)
-var x = $("a:contains('Log out')").attr('href');
+var logout_exps=new Array("Sign Out","Sign out","sign out","signout","Signout","logout","log out","Logout","Log Out","Log out");
+for(i=0;i<logout_exps.length;i++)
+{
+var x = $("a:contains('" + logout_exps[i] + "')");
+if (x.length != 0)
+break;
+}
 console.log(x);
-if (x)
-document.location.href = x;
+if (x.length!=0)
+document.location.href = x.attr('href');
 
 
 
